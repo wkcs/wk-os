fb9e6f200f4e0fd8628c6aa0eb4fb557e495caa3
wkcs <hqh2030@gmail.com>
2018-12-02 11:25:15 +0800

    准备添加设备树

---
+++ include/wk/dt.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2018 胡启航<Hu Qihang>
+ *
+ * Author: wkcs
+ * 
+ * Email: hqh2030@gmail.com, huqihan@live.com
+ */
+
+#ifndef __WK_DT_H__
+#define __WK_DT_H__
+
+#include <asm/types.h>
+#include <wk/list.h>
+
+struct device_node {
+
+};
+
+#endif
